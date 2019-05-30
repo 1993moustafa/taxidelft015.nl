06c1b1f4287fcf72785022887848ba803cb35810
Moustafa Omar <1993moustafa@gmail.com>
2019-05-30 11:21:19 +0200

    change navbar brand

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg">
   <div class="container">
-    <a class="navbar-brand" href="#">Hello World</a>
+    <a class="navbar-brand" href="#">Hello test</a>
     <button class="navbar-toggler border-0 menu btn14" type="button" data-menu="14" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <div class="icon"></div>
     </button>
