b6017ab2e78bb0cb11f634ad15b26e2866b64626
Moustafa Omar <1993moustafa@gmail.com>
2019-06-04 05:34:36 +0200

    call us button

--- components/navbar.php
+++ components/navbar.php
@@ -1,76 +1,3 @@
-<!--
-<nav class="navbar navbar-expand-lg navbar-shark bg-shark">
-  <div class="container">
-    <a class="navbar-brand" href="#">Taxidelft015</a>
-    <button class="navbar-toggler border-0 menu btn14" type="button" data-menu="14" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <div class="icon"></div>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <hr>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown megamenu-li">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu megamenu" aria-labelledby="navbarDropdown">
-            <div class="container">
-              <div class="row">
-                <div class="col-12 col-lg-3">
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Leiden CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Rotterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag HS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Amsterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Schiedam CS</a>
-                  <hr />
-                </div>
-                <div class="col-12 col-lg-3">
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Leiden CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Rotterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag HS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Amsterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Schiedam CS</a>
-                  <hr />
-                </div>
-                <div class="col-12 col-lg-3">
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Leiden CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Rotterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag HS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Amsterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Schiedam CS</a>
-                  <hr />
-                </div>
-                <div class="col-12 col-lg-3">
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Leiden CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Rotterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag HS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Den Haag CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Amsterdam CS</a>
-                  <a class="dropdown-item p-0 py-2" href="#">Taxi naar Schiedam CS</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
--->
-
-
 <nav class="navbar navbar-expand-md navbar-shark bg-shark p-lg-0" role="navigation">
   <div class="container">
   <!-- Brand and toggle get grouped for better mobile display -->
@@ -94,3 +21,11 @@
 		?>
 	</div>
 </nav>
+
+<a href="tel:+31640253134" mypage="" class="call-now" rel="nofollow">
+  <div class="mypage-alo-phone">
+    <div class="animated infinite zoomIn mypage-alo-ph-circle"></div>
+    <div class="animated infinite pulse mypage-alo-ph-circle-fill"></div>
+    <div class="animated infinite tada mypage-alo-ph-img-circle"></div>
+  </div>
+</a>
