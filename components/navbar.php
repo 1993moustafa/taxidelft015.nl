82fa5b17cae6c1d0d9cf30f63825159dc3adc182
Moustafa Omar <1993moustafa@gmail.com>
2019-07-01 10:08:35 +0200

    Navbar shark

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dodgerblue bg-dodgerblue p-lg-0" role="navigation">
+<nav class="navbar navbar-expand-md navbar-shark bg-shark p-lg-0" role="navigation">
   <div class="container">
   <!-- Brand and toggle get grouped for better mobile display -->
   <a class="navbar-brand" href="/">
